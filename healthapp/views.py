9222d7fb97341a61cf10e0b5fa99a0d0515dac5c
Manirabona Patience <hseal419@gmail.com>
2024-01-30 10:20:22 +0200

    chore: added login & signup pages

--- healthapp/views.py
+++ healthapp/views.py
@@ -4,5 +4,10 @@ from django.http import HttpResponse
 from.models import*
 
 def index(request):
-    
-    return render(request, "index.html",)
+    return render(request, "index.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def signup(request):
+    return render(request, "signup.html")
